e56b1066b7610ea8d1ee61bbd36e35f1483dd255
NitricXd <Nitricxd@gmail.com>
2022-06-17 18:22:42 +0530

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+API_ID = os.getenv("API_ID", "11508502").strip()
+API_HASH = os.getenv("API_HASH", "3e6e5be80d5c89ff19b5e5fb7bfaa0f4").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "5443525365:AAGGUw3_qmi2PzfRG5GGS9wCIVAwH98W4xI").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+MUST_JOIN = os.getenv("MUST_JOIN", "TheXCodeTeam")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
